6d45b3803ba7cde214a4f3ada3ec44a402756c3a
Thomas Perl <m@thp.io>
2016-07-12 20:58:17 +0200

    urlwatch 2.4

--- setup.py
+++ setup.py
@@ -1,43 +1,28 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Minimalistic, automatic setup.py file for Python modules
-# Copyright (c) 2008-2016 Thomas Perl <thp.io/about>
+#!/usr/bin/env python3
 
 from setuptools import setup
 
 import os
 import re
-import sys
 
-PACKAGE_NAME = 'urlwatch'
-DEPENDENCIES = ['minidb', 'PyYAML', 'requests']
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-if os.path.normpath(os.getcwd()) != os.path.normpath(HERE):
-    print('You must run {} inside {} (cwd={})'.format(os.path.basename(__file__), HERE, os.getcwd()))
-    sys.exit(1)
-
-# Assumptions:
-#  1. Package name equals main script file name (and only one script)
-#  2. Main script contains docstring + dunder-{author, license, url, version}
-#  3. Data files are in "share/", will be installed in $(PREFIX)/share
-#  4. Packages are in "lib/", no modules
-
-main_py = open(os.path.join(HERE, 'lib', PACKAGE_NAME, '__init__.py')).read()
+main_py = open(os.path.join('lib', 'urlwatch', '__init__.py')).read()
 m = dict(re.findall("\n__([a-z]+)__ = '([^']+)'", main_py))
 docs = re.findall('"""(.*?)"""', main_py, re.DOTALL)
 
-m['name'] = PACKAGE_NAME
+m['name'] = 'urlwatch'
 m['author'], m['author_email'] = re.match(r'(.*) <(.*)>', m['author']).groups()
 m['description'], m['long_description'] = docs[0].strip().split('\n\n', 1)
-m['download_url'] = m['url'] + PACKAGE_NAME + '-' + m['version'] + '.tar.gz'
-
-m['scripts'] = [os.path.join(HERE, PACKAGE_NAME)]
-m['package_dir'] = {'': os.path.join(HERE, 'lib')}
-m['packages'] = ['.'.join(os.path.relpath(dirname, HERE).split(os.sep)[1:])
-                 for dirname, _, files in os.walk(os.path.join(HERE, 'lib')) if '__init__.py' in files]
-m['data_files'] = [(os.path.relpath(dirname, HERE), [os.path.join(os.path.relpath(dirname, HERE), fn) for fn in files])
-                   for dirname, _, files in os.walk(os.path.join(HERE, 'share')) if files]
-m['install_requires'] = DEPENDENCIES
+m['download_url'] = '{url}urlwatch-{version}.tar.gz'.format(**m)
+m['install_requires'] = ['minidb', 'PyYAML', 'requests']
+m['scripts'] = ['urlwatch']
+m['package_dir'] = {'': 'lib'}
+m['packages'] = ['urlwatch']
+m['data_files'] = [
+    ('share/man/man1', ['share/man/man1/urlwatch.1']),
+    ('share/urlwatch/examples', [
+        'share/urlwatch/examples/hooks.py.example',
+        'share/urlwatch/examples/urls.yaml.example',
+    ]),
+]
 
 setup(**m)
