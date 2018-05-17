60ccad0cf67f6b76de459acd0455849b725690b4
Thomas Perl <m@thp.io>
2018-05-17 23:14:03 +0200

    Remove download_url from setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ docs = re.findall('"""(.*?)"""', main_py, re.DOTALL)
 m['name'] = 'urlwatch'
 m['author'], m['author_email'] = re.match(r'(.*) <(.*)>', m['author']).groups()
 m['description'], m['long_description'] = docs[0].strip().split('\n\n', 1)
-m['download_url'] = '{url}urlwatch-{version}.tar.gz'.format(**m)
 m['install_requires'] = ['minidb', 'PyYAML', 'requests', 'keyring', 'pycodestyle', 'appdirs']
 m['scripts'] = ['urlwatch']
 m['package_dir'] = {'': 'lib'}
