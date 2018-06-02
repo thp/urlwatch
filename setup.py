651f9ae5b4b6b479f7ec4e7168faafe030e0bae7
Thomas Perl <m@thp.io>
2018-06-02 12:41:23 +0200

    Prevent installation on Python 2 (Fixes #244)

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from setuptools import setup
 
 import os
 import re
+import sys
 
 main_py = open(os.path.join('lib', 'urlwatch', '__init__.py')).read()
 m = dict(re.findall("\n__([a-z]+)__ = '([^']+)'", main_py))
 docs = re.findall('"""(.*?)"""', main_py, re.DOTALL)
+
+if sys.version_info < (3, 3):
+    sys.exit('urlwatch requires Python 3.3 or newer')
 
 m['name'] = 'urlwatch'
 m['author'], m['author_email'] = re.match(r'(.*) <(.*)>', m['author']).groups()
@@ -16,6 +20,7 @@ m['install_requires'] = ['minidb', 'PyYAML', 'requests', 'keyring', 'pycodestyle
 m['scripts'] = ['urlwatch']
 m['package_dir'] = {'': 'lib'}
 m['packages'] = ['urlwatch']
+m['python_requires'] = '>3.3.0'
 m['data_files'] = [
     ('share/man/man1', ['share/man/man1/urlwatch.1']),
     ('share/urlwatch/examples', [
