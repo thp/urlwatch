a2e0f8c04c13cb450e028df4249506121097297b
cfbao <cfbao.is@gmail.com>
2019-01-06 13:24:31 -0500

    Unify filter tests
    Use a test generator and a YAML file as a general easy-to-use framework for all filter tests.
    Add tests for all named filter kinds.

--- test/test_filters.py
+++ test/test_filters.py
@@ -1,118 +1,38 @@
-from urlwatch.filters import GetElementById
-from urlwatch.filters import GetElementByTag
-from urlwatch.filters import JsonFormatFilter
-from urlwatch.filters import XPathFilter
-from urlwatch.filters import CssFilter
-
+import os
+import logging
+import yaml
+from urlwatch.filters import FilterBase
 from nose.tools import eq_
 
-
-def test_get_element_by_id():
-    get_element_by_id = GetElementById(None, None)
-    result = get_element_by_id.filter("""
-    <html><head></head><body>
-    <div id="foo">asdf <span>bar</span></div>
-    <div id="bar">asdf <span>bar</span> hoho</div>
-    </body></html>
-    """, 'bar')
-    print(result)
-    eq_(result, '<div id="bar">asdf <span>bar</span> hoho</div>')
+logger = logging.getLogger(__name__)
 
 
-def test_get_element_by_tag():
-    get_element_by_tag = GetElementByTag(None, None)
-    result = get_element_by_tag.filter("""
-    <html><head></head><body>foo</body></html>
-    """, 'body')
-    print(result)
-    eq_(result, '<body>foo</body>')
+def test_filters():
+    def check_filter(test_name):
+        filter = filter_tests[test_name]['filter']
+        data = filter_tests[test_name]['data']
+        expected_result = filter_tests[test_name]['expected_result']
+        if isinstance(filter, dict):
+            key = next(iter(filter))
+            kind, subfilter = key, filter[key]
+        elif isinstance(filter, str):
+            if ',' in filter:
+                raise ValueError('Only single filter allowed in this test')
+            elif ':' in filter:
+                kind, subfilter = filter.split(':', 1)
+            else:
+                kind = filter
+                subfilter = None
+        logger.info('filter kind: %s, subfilter: %s', kind, subfilter)
+        filtercls = FilterBase.__subclasses__.get(kind)
+        if filtercls is None:
+            raise ValueError('Unknown filter kind: %s:%s' % (filter_kind, subfilter))
+        result = filtercls(None, None).filter(data, subfilter)
+        logger.debug('Expected result:\n%s', expected_result)
+        logger.debug('Actual result:\n%s', result)
+        eq_(result, expected_result)
 
-
-def test_get_element_by_tag_nested():
-    get_element_by_tag = GetElementByTag(None, None)
-    result = get_element_by_tag.filter("""
-    <html><head></head><body>
-    <div>foo</div>
-    <div>bar</div>
-    </body></html>
-    """, 'div')
-    print(result)
-    eq_(result, """<div>foo</div><div>bar</div>""")
-
-
-def test_json_format_filter():
-    json_format_filter = JsonFormatFilter(None, None)
-    result = json_format_filter.filter(
-        """{"field1": {"f1.1": "value"},"field2": "value"}""")
-    print(result)
-    eq_(result, """{
-    "field1": {
-        "f1.1": "value"
-    },
-    "field2": "value"
-}""")
-
-
-def test_json_format_filter_subfilter():
-    json_format_filter = JsonFormatFilter(None, None)
-    result = json_format_filter.filter(
-        """{"field1": {"f1.1": "value"},"field2": "value"}""", "2")
-    print(result)
-    eq_(result, """{
-  "field1": {
-    "f1.1": "value"
-  },
-  "field2": "value"
-}""")
-
-
-def test_xpath_elements():
-    xpath_filter = XPathFilter(None, None)
-    result = xpath_filter.filter("""
-    <html><head></head><body>
-    abc
-    <div>foo</div>
-    lmn
-    <span id="bar">bar</span>
-    xyz
-    </body></html>
-    """, "//div | //*[@id='bar']")
-    print(result)
-    eq_(result, """<div>foo</div>
-
-<span id="bar">bar</span>
-""")
-
-
-def test_xpath_text():
-    xpath_filter = XPathFilter(None, None)
-    result = xpath_filter.filter("""
-    <html><head></head><body>
-    abc
-    <div>foo</div>
-    lmn
-    <span id="bar">bar</span>
-    xyz
-    </body></html>
-    """, '//div/text() | //span/@id')
-    print(result)
-    eq_(result, """foo
-bar""")
-
-
-def test_css():
-    css_filter = CssFilter(None, None)
-    result = css_filter.filter("""
-    <html><head></head><body>
-    abc
-    <div>foo</div>
-    lmn
-    <span id="bar">bar</span>
-    xyz
-    </body></html>
-    """, 'div, span')
-    print(result)
-    eq_(result, """<div>foo</div>
-
-<span id="bar">bar</span>
-""")
+    with open(os.path.join(os.path.dirname(__file__), 'data/filter_tests.yaml'), 'r', encoding='utf8') as fp:
+        filter_tests = yaml.load(fp)
+    for test_name in filter_tests:
+        yield check_filter, test_name
