74bfe0210cd222882ff9f71c13e082c8d6b51642
mborsetti <26698111+mborsetti@users.noreply.github.com>
2019-03-25 15:32:06 -0700

    Load smtp username

--- lib/urlwatch/storage.py
+++ lib/urlwatch/storage.py
@@ -78,6 +78,7 @@ DEFAULT_CONFIG = {
             'method': 'smtp',
             'smtp': {
                 'host': 'localhost',
+                'smtpuser': '',
                 'port': 25,
                 'starttls': True,
                 'keyring': True,
