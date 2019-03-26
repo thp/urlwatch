9d82b7ff2e37de7286958c85b5ba36beee153c6f
mborsetti <26698111+mborsetti@users.noreply.github.com>
2019-03-25 19:29:46 -0700

    Update reporters.py

--- lib/urlwatch/reporters.py
+++ lib/urlwatch/reporters.py
@@ -394,9 +394,7 @@ class EMailReporter(TextReporter):
             logger.debug('Not sending e-mail (no changes)')
             return
         if self.config['method'] == "smtp":
-            smtp_user = self.config['smtp']['smtpuser']
-            if not smtp_user:
-                smtp_user = self.config['smtp'].get('user', self.config['from'])
+            smtp_user = self.config['smtp'].get('user', self.config['from'])
             mailer = SMTPMailer(smtp_user, self.config['smtp']['host'], self.config['smtp']['port'],
                                 self.config['smtp']['starttls'], self.config['smtp']['keyring'])
         elif self.config['method'] == "sendmail":
