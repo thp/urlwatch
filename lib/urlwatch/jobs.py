90821bdffb258adf598acb5c1dfe01bb7eb7dad9
mborsetti <mike@borsetti.com>
2020-05-06 20:15:44 +0800

    now only looks at first filter

--- lib/urlwatch/jobs.py
+++ lib/urlwatch/jobs.py
@@ -202,7 +202,7 @@ class ShellJob(Job):
         # If the first filter is a bytes filter, return content in bytes instead of in unicode
         # as that's what's required by the library used by that filter
         if self.filter and (FilterBase.is_bytes_filter(self.filter)
-                            or any(FilterBase.is_bytes_filter(filter) for filter in self.filter[0])):
+                            or FilterBase.is_bytes_filter(next(iter(self.filter[0])))):
             return stdout_data
 
         return stdout_data.decode('utf-8')
@@ -293,8 +293,10 @@ class UrlJob(Job):
 
         # If the first filter is a bytes filter, return content in bytes instead of in unicode
         # as that's what's required by the library used by that filter
+        print(f'{next(iter(self.filter[0]))=}')
+        print(f'{list(self.filter[0])=}')
         if self.filter and (FilterBase.is_bytes_filter(self.filter)
-                            or any(FilterBase.is_bytes_filter(filter) for filter in self.filter[0])):
+                            or FilterBase.is_bytes_filter(next(iter(self.filter[0])))):
             return response.content
 
         # If we can't find the encoding in the headers, requests gets all
